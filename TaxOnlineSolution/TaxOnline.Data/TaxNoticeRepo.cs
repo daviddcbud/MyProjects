b4d7d08812911d2928c46e9e1049697329040773
daviddcbud <daviddcbud@hotmail.com>
2013-08-29 07:46:07 -0500

    more work

---
+++ TaxOnlineSolution/TaxOnline.Data/TaxNoticeRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using TaxOnline.Models;
+
+namespace TaxOnline.Data
+{
+    public class TaxNoticeRepo
+    {
+        DataContext _context;
+        public TaxNoticeRepo(IPrincipal user)
+        {
+             _context=new DataContext();
+            UserId = user.Identity.Name;
+        }
+        public string UserId { get; private set; }
+        public TaxNotice GetTaxNotice(int id)
+        {
+            return _context.TaxNotices.Include("Taxpayer").Where(x => x.Id == id).Single();
+        }
+    }
+}
---
+++ TaxOnlineSolution/TaxOnline.Web/Controllers/TaxNoticeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaxOnline.Data;
+using TaxOnline.Models;
+
+namespace TaxOnline.Web.Controllers
+{
+    [Authorize]
+    public class TaxNoticeController : ApiController
+    {
+        TaxNoticeRepo _repo;
+        public TaxNoticeController()
+        {
+            _repo = new TaxNoticeRepo(User);
+        }
+        // GET api/taxnotice
+        public IEnumerable<TaxNotice>  Get( )
+        {
+            
+            return null;
+        }
+
+        // GET api/taxnotice/5
+        public TaxNotice Get(int id)
+        {
+            var taxnotice = _repo.GetTaxNotice(id);
+            return taxnotice;
+        }
+
+        // POST api/taxnotice
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/taxnotice/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/taxnotice/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
